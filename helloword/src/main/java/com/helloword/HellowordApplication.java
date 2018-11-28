c0a8ec921528d878af36b3140b129f9ee3079a23
abh1906 <jain.abhinav2406@gmail.com>
2018-11-28 14:27:06 +0530

    v2

---
+++ helloword/src/main/java/com/helloword/HellowordApplication.java
@@ -0,0 +1,17 @@
+package com.helloword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellowordApplication.class, args);
+		helloWord();
+	}
+	
+	public static void helloWord(){
+		System.out.println("Hello Word!!!");
+	}
+}
