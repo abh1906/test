3a01a22d2c455a4804707e5c0404c99319ec453c
abh1906 <jain.abhinav2406@gmail.com>
2018-11-28 16:05:46 +0530

    corrected

--- helloword/src/main/java/com/helloword/HellowordApplication.java
+++ helloword/src/main/java/com/helloword/HellowordApplication.java
@@ -6,12 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HellowordApplication {
 
-	publi static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(HellowordApplication.class, args);
 		helloWord();
 	}
 	
 	public static void helloWord(){
-		System.out.println("Hello Word!!!")
+		System.out.println("Hello Word!!!");
 	}
 }
